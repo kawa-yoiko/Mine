0f25656d44d5850f4c66a7d7c706998d8790ed45
Shiqing <shiqing-thu18@yandex.com>
2021-05-15 14:25:12 +0800

    Use Node.js for testing

---
+++ server/tests/test.js
@@ -0,0 +1,136 @@
+const http = require('http');
+
+let jarId = 0;
+const cookieJar = [{}, {}, {}, {}, {}];
+const getCookies = () => {
+  const jar = cookieJar[jarId];
+  let s = [];
+  for (let key in jar) s.push(`${key}=${jar[key]}`);
+  return s.join('; ');
+};
+
+const asyncGet = (url, params) => new Promise((resolve, reject) => {
+  if (params) url += '?' + (new URLSearchParams(params)).toString();
+  const headers = {
+    'Cookie': getCookies(),
+  };
+  const req = http.request(url, { headers }, (res) => {
+    res.setEncoding('utf8');
+    const data = [];
+    res.on('data', (chunk) => data.push(chunk));
+    res.on('end', () => resolve([res.statusCode, data.join(''), res.headers]));
+  });
+  req.end();
+});
+
+const asyncPost = (url, params) => new Promise((resolve, reject) => {
+  const body = (new URLSearchParams(params)).toString();
+  const opt = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': Buffer.byteLength(body),
+      'Cookie': getCookies(),
+    },
+  };
+  const req = http.request(url, opt, (res) => {
+    res.setEncoding('utf8');
+    const data = [];
+    res.on('data', (chunk) => data.push(chunk));
+    res.on('end', () => resolve([res.statusCode, data.join(''), res.headers]));
+  });
+  req.write(body);
+  req.end();
+});
+
+const objEqual = (a, b) => {
+  if (a === null || b === null) return true;
+  if (typeof a !== typeof b) return false;
+  if (typeof a !== 'object') return a === b;
+
+  for (let key in a) if (!objEqual(a[key], b[key])) return false;
+  for (let key in b) if (!objEqual(a[key], b[key])) return false;
+  return true;
+};
+
+let total = 0, pass = 0;
+
+const check = async (method, url, params, expect, expect_status) => {
+  const baseUrl = 'http://localhost:2317';
+  const info = `${method + ' '.repeat(4 - method.length)} ` +
+    `${url + ' '.repeat(20 - url.length)} | `;
+  const [status, response, headers] =
+    await (method === 'GET' ? asyncGet : asyncPost)(baseUrl + url, params);
+
+  // Handle cookies
+  const setCookie = headers['set-cookie'];
+  if (setCookie) for (let i = 0; i < setCookie.length; i++) {
+    let s = setCookie[i];
+    const p = s.indexOf(';');
+    if (p !== -1) s = s.substring(0, p);
+    const [key, value] = s.split('=');
+    cookieJar[jarId][key] = value;
+  }
+
+  let actual = undefined;
+  let result = '';
+  let correct = true;
+  let compare = false;
+
+  if (status !== (expect_status || 200)) {
+    result = `Incorrect status ${status}`;
+    correct = false;
+    compare = true;
+  }
+  try {
+    actual = JSON.parse(response);
+  } catch (e) {
+    if (expect !== undefined) {
+      result = `Incorrect JSON format ${response.trimEnd()}`;
+      correct = false;
+    } else {
+      result = `Correct empty response`;
+    }
+  }
+  if (correct && expect !== undefined) {
+    if (objEqual(expect, actual)) {
+      const s = JSON.stringify(actual);
+      result = `Correct ${status} ${s.substring(0, 50)}${s.length > 50 ? '...' : ''}`;
+    } else {
+      result = `Incorrect response content`;
+      correct = false;
+      compare = true;
+    }
+  }
+
+  console.log(info + result);
+  if (!correct) {
+    console.log('Params: ', params);
+    if (compare) {
+      console.log('Expect: ', expect);
+      console.log('Actual: ', actual);
+      console.log();
+    }
+  }
+
+  total += 1;
+  pass += (correct ? 1 : 0);
+
+  return actual;
+};
+
+(async () => {
+  console.log('======== Sign up ========');
+  await check('POST', '/reset')
+  await check('POST', '/signup', {nickname: 'kayuyuko1', email: 'kyyk1@kawa..moe', password: '888888'}, {error: 1}, 400)
+  await check('POST', '/signup', {nickname: 'kayuyuko1', email: 'kyyk1@kawa.moe', password: '哈哈哈哈哈哈'}, {error: 1}, 400)
+  await check('POST', '/signup', {nickname: 'kayuyuko1', email: 'kyyk1@kawa.moe', password: '888'}, {error: 1}, 400)
+  await check('POST', '/signup', {nickname: 'kayuyuko1', email: 'kyyk1@kawa.moe', password: '888888'}, {error: 0}, 200)
+  await check('POST', '/signup', {nickname: 'kayuyuko1', email: 'kyyk2@kawa.moe', password: '888888'}, {error: 2}, 400)
+  await check('POST', '/signup', {nickname: 'kayuyuko2', email: 'kyyk1@kawa.moe', password: '888888'}, {error: 3}, 400)
+  await check('POST', '/signup', {nickname: 'kayuyuko2', email: 'kyyk2@kawa.moe', password: '888888'}, {error: 0}, 200)
+  await check('POST', '/signup', {nickname: '小猫', email: 'kurikoneko@kawa.moe', password: '888888'}, {error: 1}, 400)
+  await check('POST', '/signup', {nickname: '栗小猫', email: 'kurikoneko@kawa.moe', password: '888888'}, {error: 0}, 200)
+
+  console.log(`\n${pass}/${total} passed`);
+})();
