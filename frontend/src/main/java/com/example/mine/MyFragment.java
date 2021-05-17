c5eb98d8b3522d52ee3456f29bebbbf885a5ee93
kurikoneko <yangxuran1023@163.com>
2021-05-17 17:41:50 +0800

    delete error version`

--- frontend/src/main/java/com/example/mine/MyFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.mine;
-
-import androidx.fragment.app.Fragment;
-
-public class MyFragment  extends Fragment {
-    public MyFragment() {}
-}
--- frontend/src/main/java/com/example/mine/TMP.java
+++
@@ -1,8 +0,0 @@
-package com.example.mine;
-
-import android.widget.ImageView;
-import android.widget.TextView;
-
-public class TMP {
-
-}
