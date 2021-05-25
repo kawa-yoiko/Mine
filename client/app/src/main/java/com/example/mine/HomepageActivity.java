c10993ec16ac3eae6e3f3a29d22a934774dac8d0
Shiqing <shiqing-thu18@yandex.com>
2021-05-26 01:55:26 +0800

    Display collection list in user home page

--- client/app/src/main/java/com/example/mine/HomepageActivity.java
+++ client/app/src/main/java/com/example/mine/HomepageActivity.java
@@ -1,11 +1,17 @@
 package com.example.mine;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
@@ -21,13 +27,28 @@ public class HomepageActivity extends AppCompatActivity{
     private List<Fragment> fragmentList = new ArrayList<>(2);
     private String[] tabText = {"作品", "合集"};
 
+    public static class CollectionListFragment extends Fragment {
+        public CollectionListFragment() {
+            super(R.layout.empty_constraintlayout);
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+            ConstraintLayout layout = (ConstraintLayout) view.findViewById(R.id.container);
+            layout.addView(CollectionListView.inflate(layout.getContext(), (User.CollectionBrief sel, Boolean init) -> {
+                android.util.Log.d("HomepageActivity", "Clicked collection " + sel.title);
+            }));
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_homepage);
         Fragment squareFragment = new SquareFragment();
-        Fragment discoverFragment = new DiscoverFragment();
+        Fragment discoverFragment = new CollectionListFragment();
         fragmentList.add(squareFragment);
         fragmentList.add(discoverFragment);
         viewPager = findViewById(R.id.view_pager);
