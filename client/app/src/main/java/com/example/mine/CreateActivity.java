6a8bfae0c3a92204a2779a0ff4cfbd40bc872ef0
Shiqing <shiqing-thu18@yandex.com>
2021-06-02 16:20:34 +0800

    Preview and upload video

--- client/app/src/main/java/com/example/mine/CreateActivity.java
+++ client/app/src/main/java/com/example/mine/CreateActivity.java
@@ -1,6 +1,9 @@
 package com.example.mine;
 
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
@@ -16,6 +19,10 @@ import androidx.recyclerview.widget.ConcatAdapter;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
+import android.os.ParcelFileDescriptor;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.util.Pair;
 import android.view.Gravity;
@@ -30,11 +37,13 @@ import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileDescriptor;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -45,6 +54,8 @@ public class CreateActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     ArrayList<ImageItem> imageItems = new ArrayList<>(0);
     WidthEqualsHeightImageView addImage;
+    String singleMediaPath;
+    VideoView videoView;
     private static int IMAGE_PICKER = 0;
     private static int VIDEO_PICKER = 2;
 
@@ -92,6 +103,7 @@ public class CreateActivity extends AppCompatActivity {
 //            recyclerView.setAdapter(new ImagePickerAdapter(imageItems));
         } else if (createType.equals("video")) {
             createAreaView = View.inflate(this.getBaseContext(), R.layout.create_area_video, null);
+            this.videoView = (VideoView)createAreaView.findViewById(R.id.video_view);
             ((ImageButton) createAreaView.findViewById(R.id.btn_select)).setOnClickListener((View v) -> {
                 Intent videoIntent = new Intent();
                 videoIntent.setType("video/*");
@@ -226,10 +238,34 @@ public class CreateActivity extends AppCompatActivity {
                         }
                     });
                 }
+            } else if (createType.equals("video")) {
+                Log.d("CreateActivity", "video path: " + singleMediaPath);
+                ServerReq.uploadFile("/upload", new File(singleMediaPath), (Long len, Long sent) -> {
+                    double progress = (double)sent / len;
+                    Log.d("CreateActivity", "video upload progress = " + progress);
+                    ((ProgressBar) cv.findViewById(R.id.progress)).setMax(1000);
+                    ((ProgressBar) cv.findViewById(R.id.progress)).setProgress((int) Math.round(1000 * progress), true);
+                }, (JSONObject obj) -> {
+                    String id = "";
+                    try {
+                        id = obj.getJSONArray("ids").getString(0);
+                    } catch (JSONException e) {
+                        Log.e("CreateActivity", e.toString());
+                    }
+                    Log.d("CreateActivity", "upload id = " + id);
+                    ServerReq.postJson("/post/new", List.of(
+                            new Pair<>("type", "2"),
+                            new Pair<>("caption", ((EditText) createAreaView.findViewById(R.id.caption_input)).getText().toString()),
+                            new Pair<>("contents", id),
+                            new Pair<>("collection", String.valueOf(collection.id)),
+                            new Pair<>("tags", tags)
+                    ), goToCreatedPost);
+                });
             }
         });
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -243,7 +279,144 @@ public class CreateActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 Uri selectedUri = data.getData();
                 Log.d("CreateAcvitity", selectedUri.toString());
+                singleMediaPath = ContentProviderUtils.getPath(this.getBaseContext(), selectedUri);
+                Log.d("CreateAcvitity", singleMediaPath);
+                videoView.setVisibility(View.VISIBLE);
+                videoView.setVideoURI(selectedUri);
+                videoView.start();
             }
+        }
+    }
+
+    private static class ContentProviderUtils {
+        /**
+         * Get a file path from a Uri. This will get the the path for Storage Access
+         * Framework Documents, as well as the _data field for the MediaStore and
+         * other file-based ContentProviders.
+         *
+         * @param context The context.
+         * @param uri The Uri to query.
+         * @author paulburke
+         */
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        public static String getPath(final Context context, final Uri uri) {
+
+            final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+            // DocumentProvider
+            if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+                // ExternalStorageProvider
+                if (isExternalStorageDocument(uri)) {
+                    final String docId = DocumentsContract.getDocumentId(uri);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+
+                    if ("primary".equalsIgnoreCase(type)) {
+                        return Environment.getExternalStorageDirectory() + "/" + split[1];
+                    }
+
+                    // TODO handle non-primary volumes
+                }
+                // DownloadsProvider
+                else if (isDownloadsDocument(uri)) {
+
+                    final String id = DocumentsContract.getDocumentId(uri);
+                    final Uri contentUri = ContentUris.withAppendedId(
+                            Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                    return getDataColumn(context, contentUri, null, null);
+                }
+                // MediaProvider
+                else if (isMediaDocument(uri)) {
+                    final String docId = DocumentsContract.getDocumentId(uri);
+                    final String[] split = docId.split(":");
+                    final String type = split[0];
+
+                    Uri contentUri = null;
+                    if ("image".equals(type)) {
+                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("video".equals(type)) {
+                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                    } else if ("audio".equals(type)) {
+                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    }
+
+                    final String selection = "_id=?";
+                    final String[] selectionArgs = new String[] {
+                            split[1]
+                    };
+
+                    return getDataColumn(context, contentUri, selection, selectionArgs);
+                }
+            }
+            // MediaStore (and general)
+            else if ("content".equalsIgnoreCase(uri.getScheme())) {
+                return getDataColumn(context, uri, null, null);
+            }
+            // File
+            else if ("file".equalsIgnoreCase(uri.getScheme())) {
+                return uri.getPath();
+            }
+
+            return null;
+        }
+
+        /**
+         * Get the value of the data column for this Uri. This is useful for
+         * MediaStore Uris, and other file-based ContentProviders.
+         *
+         * @param context The context.
+         * @param uri The Uri to query.
+         * @param selection (Optional) Filter used in the query.
+         * @param selectionArgs (Optional) Selection arguments used in the query.
+         * @return The value of the _data column, which is typically a file path.
+         */
+        public static String getDataColumn(Context context, Uri uri, String selection,
+                                           String[] selectionArgs) {
+
+            Cursor cursor = null;
+            final String column = "_data";
+            final String[] projection = {
+                    column
+            };
+
+            try {
+                cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                        null);
+                if (cursor != null && cursor.moveToFirst()) {
+                    final int column_index = cursor.getColumnIndexOrThrow(column);
+                    return cursor.getString(column_index);
+                }
+            } finally {
+                if (cursor != null)
+                    cursor.close();
+            }
+            return null;
+        }
+
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is ExternalStorageProvider.
+         */
+        public static boolean isExternalStorageDocument(Uri uri) {
+            return "com.android.externalstorage.documents".equals(uri.getAuthority());
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is DownloadsProvider.
+         */
+        public static boolean isDownloadsDocument(Uri uri) {
+            return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+        }
+
+        /**
+         * @param uri The Uri to check.
+         * @return Whether the Uri authority is MediaProvider.
+         */
+        public static boolean isMediaDocument(Uri uri) {
+            return "com.android.providers.media.documents".equals(uri.getAuthority());
         }
     }
 }
