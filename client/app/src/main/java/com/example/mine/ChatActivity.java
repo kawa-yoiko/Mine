e9d67b2ed9cf5a5b1339e02efc4904a98e3b9ba0
Shiqing <shiqing-thu18@yandex.com>
2021-06-23 14:49:26 +0800

    Refresh messages in chat page

--- client/app/src/main/java/com/example/mine/ChatActivity.java
+++ client/app/src/main/java/com/example/mine/ChatActivity.java
@@ -23,18 +23,27 @@ import androidx.recyclerview.widget.RecyclerView;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class ChatActivity extends AppCompatActivity {
     RecyclerView recyclerView;
+    private Timer timer;
+
+    private String otherNickname, otherAvatar;
+    private LinkedList<Chat> chats;
+    private ChatAdapter adapter;
+
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String otherNickname = getIntent().getStringExtra("username");
-        String otherAvatar = getIntent().getStringExtra("other_avatar");
+        otherNickname = getIntent().getStringExtra("username");
+        otherAvatar = getIntent().getStringExtra("other_avatar");
 
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_chat);
@@ -42,9 +51,9 @@ public class ChatActivity extends AppCompatActivity {
         ((TextView) findViewById(R.id.nickname)).setText(otherNickname);
 
         recyclerView = findViewById(R.id.recyclerview);
-        LinkedList<Chat> chats = new LinkedList<>();
+        chats = new LinkedList<>();
 
-        ChatAdapter adapter = new ChatAdapter(otherAvatar, chats);
+        adapter = new ChatAdapter(otherAvatar, chats);
         recyclerView.setAdapter(adapter);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getBaseContext());
         layoutManager.setReverseLayout(true);
@@ -60,17 +69,15 @@ public class ChatActivity extends AppCompatActivity {
         });
 
         Handler handler = new Handler(Looper.getMainLooper());
-        ServerReq.getJsonArray("/message/with/" + otherNickname + "?start=0&count=50", (JSONArray arr) -> handler.post(() -> {
-            try {
-                for (int i = 0; i < arr.length(); i++)
-                    chats.add(new Chat(arr.getJSONObject(i)));
-                adapter.notifyDataSetChanged();
-                recyclerView.scrollToPosition(0);
-            } catch (Exception e) {
-                Log.e("ChatActivity", e.toString());
+        updateMessages(handler);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                updateMessages(handler);
             }
-        }));
-        ServerReq.post("/message/read/" + otherNickname, List.of(), (String s) -> {});
+        }, 2000, 2000);
 
         EditText commentEdit = findViewById(R.id.comment_edit);
         Button commentButton = findViewById(R.id.comment_button);
@@ -84,6 +91,7 @@ public class ChatActivity extends AppCompatActivity {
             ), (JSONObject obj) -> handler.post(() -> {
                 try {
                     chats.addFirst(new Chat(
+                            obj.getInt("id"),
                             contents,
                             DateUtils.getRelativeTimeSpanString(obj.getLong("timestamp") * 1000).toString(),
                             1
@@ -99,5 +107,24 @@ public class ChatActivity extends AppCompatActivity {
                 }
             }));
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void updateMessages(Handler handler) {
+        ServerReq.getJsonArray("/message/with/" + otherNickname + "?start=0&count=1000", (JSONArray arr) -> handler.post(() -> {
+            try {
+                LinkedList<Chat> newChats = new LinkedList<>();
+                for (int i = 0; i < arr.length(); i++)
+                    newChats.add(new Chat(arr.getJSONObject(i)));
+                if (!chats.isEmpty() && newChats.getFirst().id == chats.getFirst().id) return;
+                chats.clear();
+                chats.addAll(newChats);
+                adapter.notifyDataSetChanged();
+                recyclerView.scrollToPosition(0);
+            } catch (Exception e) {
+                Log.e("ChatActivity", e.toString());
+            }
+            ServerReq.post("/message/read/" + otherNickname, List.of(), (String s) -> {});
+        }));
     }
 }
